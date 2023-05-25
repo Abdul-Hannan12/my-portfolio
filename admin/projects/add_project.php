3b8e797218fdf27801d05e6ea99039d75589c1d2
Rocky-Khan <abdulhannan8540680@gmail.com>
2023-05-25 18:21:39 +0500

    add project page

--- admin/projects/add_project.php
+++ admin/projects/add_project.php
@@ -1,48 +1,47 @@
 <?php include '../includes/header.php' ?>
 
-<!-- Sale & Revenue Start -->
 <div class="container-fluid mt-4">
     <div class="row">
         <div class="col-sm-11 px-sm-4 mb-5">
             <div class="bg-white rounded px-md-5 px-4 py-4">
-                <h4 class="title mb-4"> Stock Managment </h4>
+                <h4 class="title mb-4"> Add Project </h4>
 
-                <form action="" id="addStock">
+                <form action="" id="addProject">
                     <div class="row">
+
+                    <div class="col-sm-12 mb-4">
+                        <!-- Uploaded image area-->
+                        <div class="image-area"><img id="imageResult" src="#" alt="" class="img-fluid rounded shadow-sm mx-auto d-block w-25"></div>
+                        <!-- Image Upload Input -->
+                        <div class="col-sm-12 mt-4">
+                            <input type="file" id="upload" name="upload" class="form-control" onchange="readURL(this);" id="upload">
+                        </div>
+
+                    </div>
                         <div class="col-sm-6 mb-4">
-                            <label for="name" class="form-label">Product name</label>
+                            <label for="pname" class="form-label">Project name</label>
                             <input type="text" class="form-control" id="pname" name="pname">
                         </div>
                         <div class="col-sm-6 mb-4">
-                            <label for="name" class="form-label">Manufacturing Company</label>
-                            <input type="text" class="form-control" id="mcompany" name="mcompany">
+                            <label for="type" class="form-label">Type</label>
+                            <select class="form-select" id="type" name="type">
+                                <option value="">Select</option>
+                                <option value="app">App</option>
+                                <option value="web">Web</option>
+                                <option value="design">Design</option>
+                                <option value="other">Other</option>
+                            </select>
+                        </div>
+                        <div class="col-sm-6 mb-4" id="urlDiv">
+                            <label for="url" class="form-label">Url</label>
+                            <input type="text" class="form-control" id="url" name="url">
                         </div>
                         <div class="col-sm-6 mb-4">
-                            <label for="name" class="form-label">Manufacturing Date</label>
-                            <input type="date" class="form-control" id="mdate" name="mdate">
-                        </div>
-                        <div class="col-sm-6 mb-4">
-                            <label for="name" class="form-label">Expiry Date</label>
-                            <input type="date" class="form-control" id="edate" name="edate">
-                        </div>
-                        <div class="col-sm-6 mb-4">
-                            <label for="name" class="form-label">Price</label>
-                            <input type="number" class="form-control" id="price" name="price">
-                        </div>
-                        <div class="col-sm-6 mb-4">
-                            <label for="name" class="form-label">Quantity</label>
-                            <input type="number" class="form-control" id="qty" name="quantity">
-                        </div>
-                        <div class="col-sm-6 mb-4">
-                            <label for="name" class="form-label">Lot Number</label>
-                            <input type="text" class="form-control" id="lot" name="lotnumber">
-                        </div>
-                        <div class="col-sm-6 mb-4">
-                            <label for="name" class="form-label">Description</label>
+                            <label for="desc" class="form-label">Description</label>
                             <textarea name="desp" rows="5" class="form-control h-auto" id="desc"></textarea>
                         </div>
 
-                        <div class="text-end"> <input type="submit" class="btn btn-primary" id="addStock" name="addStock"> </div>
+                        <div class="text-end"> <input type="submit" class="btn btn-primary" id="addProject" name="addProject"> </div>
                     </div>
                 </form>
 
@@ -50,66 +49,91 @@
         </div>
     </div>
 </div>
-<!-- Sale & Revenue End -->
-
-
 
 
 <?php include '../includes/footer.php' ?>
 
+<script>
+
+    function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#imageResult')
+                .attr('src', e.target.result);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$(function () {
+    $('#upload').on('change', function () {
+        readURL(input);
+    });
+});
+
+</script>
 
 <script>
-     $("#addStock").submit(function(event) {
+
+$("#urlDiv").hide();
+
+$("#type").change(function(e){
+    if(e.target.value == 'web' || e.target.value == 'other'){
+        $("#urlDiv").show();
+    }else{
+        $("#urlDiv").hide();
+    }
+});
+
+</script>
+
+<script>
+
+     $("#addProject").submit(function(event) {
         event.preventDefault();
-        if($("#pname").val() == ""){
-            alert("Please Enter Name");
-            return
-        }else if($("#mcompany").val() == ""){
-            alert("Please Enter manafacture company name");
-            return
-        }else if($("#mdate").val() == ""){
-            alert("Please Enter manafacture date");
-            return
-        }else if($("#edate").val() == ""){
-            alert("Please Enter expiy Date");
-            return
-        }else if($("#price").val() == ""){
-            alert("Please Enter price");
-            return
-        }else if($("#qty").val() == ""){
-            alert("Please Enter quantity");
-            return
-        }else if($("#lot").val() == ""){
-            alert("Please Enter lot-Number");
-            return
+        if($("#upload").val() == ""){
+            alert("Please Upload an Image");
+            return;
+        }else if($("#pname").val() == ""){
+            alert("Please Enter Project Name");
+            return;
+        }else if($("#type").val() == ""){
+            alert("Please Enter Type");
+            return;
+        }else if (($("#type").val() == 'web' || $("#type").val() == 'other') && ($("#url").val() == "")){
+            alert("Please Enter Url");
+            return;
         }else if($("#desc").val() == ""){
             alert("Please Enter description");
-            return
+            return;
         }else{
             $.ajax({
             type: "POST",
             url: "../api/process.php",
-            data:  "MODE=addStock&" + $("#addStock").serialize(),
+            data:  "MODE=addProject&" + $("#addProject").serialize(),
             success: function(data) {
                 var {Status} = JSON.parse(data) 
-                        if(Status == "Success"){
-                            swal(
-                            'Welldone',
-                            'Stock Added Successfully!',
-                            'success'
-                            ).then(function() {
-                                window.location.reload();
-                            });
-                           
-                        }else{
-                            swal(
-                            'Opss',
-                            'Stock couldn\'t Added Successfully!',
-                            'error'
-                            )
-                        }
+                if(Status == "Success"){
+                    swal(
+                    'Welldone',
+                    'Project Added Successfully!',
+                    'success'
+                    ).then(function() {
+                        window.location.reload();
+                    });
+                    
+                }else{
+                    swal(
+                    'Opss',
+                    'Project couldn\'t be Added',
+                    'error'
+                    )
+                }
             }
         });
         }
     });
+
 </script>
